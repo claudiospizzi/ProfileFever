241cd0500aac268baea08023b7726b7884cee762
Claudio Spizzi <claudio@spizzi.net>
2017-11-23 21:13:10 +0100

    Add DSC resources

---
+++ Modules/ProfileFever/DSCResources/ProfileFever_PSModule/Profile_PSModule.psm1
@@ -0,0 +1,204 @@
+
+Import-Module -Name 'PackageManagement' -Verbose:$false
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Version,
+
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Repository = 'PSGallery'
+    )
+
+    # Detect the latest version from the repository
+    if ($Version -eq 'latest')
+    {
+        $Version = Get-LatestModuleVersion -Name $Name -Repository $Repository
+    }
+
+    try
+    {
+        $module = Get-InstalledModule -Name $Name -RequiredVersion $Version -ErrorAction Stop
+
+        # Return the installed module
+        return @{
+            Ensure     = 'Present'
+            Name       = $module.Name
+            Version    = $module.Version
+            Repository = $module.Repository
+        }
+    }
+    catch
+    {
+        # No module was found, return absent
+        return @{
+            Ensure     = 'Absent'
+            Name       = $Name
+            Version    = $Version
+            Repository = ''
+        }
+    }
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([void])]
+    param
+    (
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Version,
+
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Repository = 'PSGallery'
+    )
+
+    # Detect the latest version from the repository
+    if ($Version -eq 'latest')
+    {
+        $Version = Get-LatestModuleVersion -Name $Name -Repository $Repository
+    }
+
+    $module = Get-TargetResource @PSBoundParameters
+
+    # The module must be present
+    if ($Ensure -eq 'Present' -and $module.Ensure -eq 'Absent')
+    {
+        # Install the target version
+        Install-ModuleVersion -Name $Name -Version $Version -Repository $Repository
+    }
+
+    # The module must be absent
+    if ($Ensure -eq 'Absent' -and $module.Ensure -eq 'Present')
+    {
+        # Remove the installed module version because it's present
+        Uninstall-ModuleVersion -Name $Name -Version $Version
+    }
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Version,
+
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Repository = 'PSGallery'
+    )
+
+    # Detect the latest version from the repository
+    if ($Version -eq 'latest')
+    {
+        $Version = Get-LatestModuleVersion -Name $Name -Repository $Repository
+    }
+
+    $module = Get-TargetResource @PSBoundParameters
+
+    if ($Ensure -eq 'Present')
+    {
+        $result = $Ensure -eq $module.Ensure -and
+                  $Repository -eq $module.Repository
+    }
+    else
+    {
+        $result = $Ensure -eq $module.Ensure
+    }
+
+    return $result
+}
+
+function Get-LatestModuleVersion($Name, $Repository)
+{
+    $cachePath = Join-Path -Path $Env:ProgramData -ChildPath 'PowerShell\Modules\ProfileFever'
+    $cacheFile = Join-Path -Path $cachePath -ChildPath "$Name.xml"
+
+    # Create the cache path
+    if (!(Test-Path -Path $cachePath))
+    {
+        New-Item -Path $cachePath -ItemType Directory | Out-Null
+    }
+
+    # Check for the cache file
+    if ((Test-Path -Path $cacheFile))
+    {
+        $cacheData = Import-Clixml -Path $cacheFile
+
+        if ($cacheData.Timestamp -gt (Get-Date).AddDays(-1))
+        {
+            Write-Verbose "Cached version found: Timestamp = $($cacheData.Timestamp), Version = $($cacheData.Version)"
+
+            return $cacheData.Version
+        }
+    }
+
+    Write-Verbose "Detect latest version in $Repository`: Name = $Name"
+
+    $module = Find-Module -Name $Name -Repository $Repository -ErrorAction Stop
+    $version = [String] $module.Version
+
+    Write-Verbose "Detect latest version in $Repository`: Name = $Name, Version = $version"
+
+    # Update cache
+    [PSCustomObject] @{ Timestamp = [DateTime]::Now; Version = $version } | Export-Clixml -Path $cacheFile
+
+    return $version
+}
+
+function Install-ModuleVersion($Name, $Version, $Repository)
+{
+    Write-Verbose "Install module from $Repository`: Name = $Name, Version = $Version"
+
+    Install-Module -Name $Name -RequiredVersion $Version -Repository $Repository -Force -AllowClobber -SkipPublisherCheck
+}
+
+function Uninstall-ModuleVersion($Name, $Version)
+{
+    Write-Verbose "Uninstall module: Name = $Name, Version = $Version"
+
+    Uninstall-Module -Name $Name -RequiredVersion $Version -Force
+}
+
+Export-ModuleMember -Function *-TargetResource
---
+++ Modules/ProfileFever/DSCResources/ProfileFever_PSRepository/Profile_PSRepository.psm1
@@ -0,0 +1,222 @@
+
+Import-Module -Name 'PackageManagement' -Verbose:$false
+
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Trusted', 'Untrusted')]
+        [System.String]
+        $InstallationPolicy = 'Trusted',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SourceLocation,
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $PublishLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptSourceLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptPublishLocation = ''
+    )
+
+    try
+    {
+        $repository = Get-PSRepository -Name $Name -ErrorAction Stop
+
+        # Return the detected repository
+        return @{
+            Name                  = $repository.Name
+            Ensure                = 'Present'
+            InstallationPolicy    = $repository.InstallationPolicy
+            SourceLocation        = $repository.SourceLocation
+            PublishLocation       = $repository.PublishLocation
+            ScriptSourceLocation  = $repository.ScriptSourceLocation
+            ScriptPublishLocation = $repository.ScriptPublishLocation
+        }
+    }
+    catch
+    {
+        # No repository was found, return absent
+        return @{
+            Name                  = $Name
+            Ensure                = 'Absent'
+            InstallationPolicy    = ''
+            SourceLocation        = ''
+            PublishLocation       = ''
+            ScriptSourceLocation  = ''
+            ScriptPublishLocation = ''
+        }
+    }
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([void])]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Trusted', 'Untrusted')]
+        [System.String]
+        $InstallationPolicy = 'Trusted',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SourceLocation,
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $PublishLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptSourceLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptPublishLocation = ''
+    )
+
+    # The repository must be registered
+    if ($Ensure -eq 'Present')
+    {
+        $repository = Get-TargetResource @PSBoundParameters
+
+        # Register the repository, if it's not already
+        if ($repository.Ensure -eq 'Absent')
+        {
+            Register-PSRepository -Name $Name -SourceLocation $SourceLocation
+        }
+
+        $repository = Get-TargetResource @PSBoundParameters
+
+        # Check each mandatory property and set the value
+        if ($InstallationPolicy -ne $repository.InstallationPolicy)
+        {
+            Set-PSRepository -Name $Name -InstallationPolicy $InstallationPolicy
+        }
+        if ($SourceLocation -ne $repository.SourceLocation)
+        {
+            Set-PSRepository -Name $Name -SourceLocation $SourceLocation
+        }
+
+        # Check the optional properties only if they were specified
+        if (![String]::IsNullOrEmpty($PublishLocation) -and $PublishLocation -ne $repository.PublishLocation)
+        {
+            Set-PSRepository -Name $Name -PublishLocation $PublishLocation
+        }
+        if (![String]::IsNullOrEmpty($ScriptSourceLocation) -and $ScriptSourceLocation -ne $repository.ScriptSourceLocation)
+        {
+            Set-PSRepository -Name $Name -ScriptSourceLocation $ScriptSourceLocation
+        }
+        if (![String]::IsNullOrEmpty($ScriptPublishLocation) -and $ScriptPublishLocation -ne $repository.ScriptPublishLocation)
+        {
+            Set-PSRepository -Name $Name -ScriptPublishLocation $ScriptPublishLocation
+        }
+    }
+
+    # The repository must be unregistered
+    if ($Ensure -eq 'Absent')
+    {
+        $repository = Get-TargetResource @PSBoundParameters
+
+        # Unregister the repository, if it's not already
+        if ($repository.Ensure -eq 'Present')
+        {
+            Unregister-PSRepository -Name $Name
+        }
+    }
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $Name,
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Present', 'Absent')]
+        [System.String]
+        $Ensure = 'Present',
+
+        [parameter(Mandatory = $false)]
+        [ValidateSet('Trusted', 'Untrusted')]
+        [System.String]
+        $InstallationPolicy = 'Trusted',
+
+        [parameter(Mandatory = $true)]
+        [System.String]
+        $SourceLocation,
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $PublishLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptSourceLocation = '',
+
+        [parameter(Mandatory = $false)]
+        [System.String]
+        $ScriptPublishLocation = ''
+    )
+
+    $repository = Get-TargetResource @PSBoundParameters
+
+    if ($Ensure -eq 'Present')
+    {
+        $result = $Ensure -eq $repository.Ensure -and $InstallationPolicy -eq $repository.InstallationPolicy -and $SourceLocation -eq $repository.SourceLocation
+
+        # Check optional parameter only, if specified
+        if (![String]::IsNullOrEmpty($PublishLocation))
+        {
+            $result = $result -and $PublishLocation -eq $repository.PublishLocation
+        }
+        if (![String]::IsNullOrEmpty($ScriptSourceLocation))
+        {
+            $result = $result -and $ScriptSourceLocation -eq $repository.ScriptSourceLocation
+        }
+        if (![String]::IsNullOrEmpty($ScriptPublishLocation))
+        {
+            $result = $result -and $ScriptPublishLocation -eq $repository.ScriptPublishLocation
+        }
+    }
+    else
+    {
+        $result = $Ensure -eq $module.Ensure
+    }
+
+    return $result
+}
