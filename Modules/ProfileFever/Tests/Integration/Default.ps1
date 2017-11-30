f1a40387b59bc79526b3edb3691219af327fda07
Claudio Spizzi <claudio@spizzi.net>
2017-11-30 14:47:13 +0100

    Update integration test

--- Modules/ProfileFever/Tests/Integration/Default.ps1
+++ Modules/ProfileFever/Tests/Integration/Default.ps1
@@ -2,123 +2,27 @@
 # Default configuration
 Configuration 'Default'
 {
-    Import-DscResource -ModuleName 'ProfileFever'
+    Import-DscResource -ModuleName 'ProfileFever' -ModuleVersion '0.0.3'
 
     Node 'localhost'
     {
-        PSRepository 'Arcade'
+        PSRepository 'Demo'
         {
-            Name               = 'Arcade'
+            Name               = 'Demo'
             InstallationPolicy = 'Trusted'
-            SourceLocation     = 'https://psgallery.arcade.ch/nuget/powershell/'
-            PublishLocation    = 'https://psgallery.arcade.ch/nuget/powershell/package/'
+            SourceLocation     = 'https://psgallery.demo.com/nuget/powershell/'
+            PublishLocation    = 'https://psgallery.demo.com/nuget/powershell/package/'
         }
 
         PSModule 'Pester'
         {
             Name    = 'Pester'
-            Version = 'latest'
+            Version = '3.4.0'
         }
 
         PSModule 'PSScriptAnalyzer'
         {
             Name    = 'PSScriptAnalyzer'
-            Version = 'latest'
-        }
-
-        PSModule 'OperationValidation'
-        {
-            Name    = 'OperationValidation'
-            Version = 'latest'
-        }
-
-        PSModule 'Plaster'
-        {
-            Name    = 'Plaster'
-            Version = 'latest'
-        }
-
-        PSModule 'psake'
-        {
-            Name    = 'psake'
-            Version = 'latest'
-        }
-
-        PSModule 'PScribo'
-        {
-            Name    = 'PScribo'
-            Version = 'latest'
-        }
-
-        PSModule 'posh-git'
-        {
-            Name    = 'posh-git'
-            Version = 'latest'
-        }
-
-        PSModule 'ActiveDirectoryFever'
-        {
-            Name    = 'ActiveDirectoryFever'
-            Version = 'latest'
-        }
-
-        PSModule 'OperationsManagerFever'
-        {
-            Name    = 'OperationsManagerFever'
-            Version = 'latest'
-        }
-
-        PSModule 'SecurityFever'
-        {
-            Name    = 'SecurityFever'
-            Version = 'latest'
-        }
-
-        PSModule 'SharePointFever'
-        {
-            Name    = 'SharePointFever'
-            Version = 'latest'
-        }
-
-        PSModule 'WindowsFever'
-        {
-            Name    = 'WindowsFever'
-            Version = 'latest'
-        }
-
-        PSModule 'ScriptLogger'
-        {
-            Name    = 'ScriptLogger'
-            Version = 'latest'
-        }
-
-        PSModule 'ScriptConfig'
-        {
-            Name    = 'ScriptConfig'
-            Version = 'latest'
-        }
-
-        PSModule 'ISEPresenter'
-        {
-            Name    = 'ISEPresenter'
-            Version = 'latest'
-        }
-
-        PSModule 'ISEScriptAnalyzerAddOn'
-        {
-            Name    = 'ISEScriptAnalyzerAddOn'
-            Version = 'latest'
-        }
-
-        PSModule 'PsISEProjectExplorer'
-        {
-            Name    = 'PsISEProjectExplorer'
-            Version = 'latest'
-        }
-
-        PSModule 'ImportExcel'
-        {
-            Name    = 'ImportExcel'
             Version = 'latest'
         }
 
@@ -128,19 +32,12 @@ Configuration 'Default'
             Version = 'latest'
         }
 
-        # PSModule 'ArcadeIcinga'
-        # {
-        #     Name       = 'ArcadeIcinga'
-        #     Version    = 'latest'
-        #     Repository = 'Arcade'
-        # }
-
-        # PSModule 'ArcadeKunden'
-        # {
-        #     Name       = 'ArcadeKunden'
-        #     Version    = 'latest'
-        #     Repository = 'Arcade'
-        # }
+        PSModule 'DemoTools'
+        {
+            Name       = 'DemoTools'
+            Version    = 'latest'
+            Repository = 'Demo'
+        }
     }
 }
 
@@ -148,4 +45,4 @@ Configuration 'Default'
 Default -OutputPath "$PSScriptRoot\Default"
 
 # Invoke the DSC configuration
-Start-DscConfiguration -Path "$PSScriptRoot\Default" -Wait -Force -Verbose
+Start-DscConfiguration -Path "$PSScriptRoot\Default" -Wait -Force -Verbose -WhatIf
