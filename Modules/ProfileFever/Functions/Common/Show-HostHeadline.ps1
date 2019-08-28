f1563986debf0ff1248aa37b343184a373c26e89
Claudio Spizzi <claudio@spizzi.net>
2019-08-28 14:33:51 +0200

    Optimite host headline

--- Modules/ProfileFever/Functions/Common/Show-HostHeadline.ps1
+++ Modules/ProfileFever/Functions/Common/Show-HostHeadline.ps1
@@ -14,50 +14,54 @@ function Show-HostHeadline
     # efficient way to test that without WMI.
     if ($Host.UI.RawUI.CursorPosition.Y -eq 0)
     {
+        $stringBuilder = [System.Text.StringBuilder]::new()
+
         # Get the PowerShell version depending on the edition
         if ($PSVersionTable.PSEdition -eq 'Core')
         {
-            $psInfo = 'PowerShell {0}' -f $PSVersionTable.PSVersion
+            $stringBuilder.AppendFormat('PowerShell {0}', $PSVersionTable.PSVersion) | Out-Null
         }
         else
         {
-            $psInfo = 'Windows PowerShell {0}.{1}' -f $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor
+            $stringBuilder.AppendFormat('Windows PowerShell {0}.{1}', $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor) | Out-Null
         }
+
+        $stringBuilder.AppendLine() | Out-Null
 
         # Get the operating system information, based on the operating system
-        $osInfo = ''
         if ([System.Environment]::OSVersion.Platform -eq 'Win32NT')
         {
-            # Get Windows version from registry. Update the object for non Windows 10 or
-            # Windows Server 2016 systems to match the same keys.
-            $osVersion = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
-            if ($null -eq $osVersion.ReleaseId)
+            # Get Windows version from registry. Update the object for non
+            # Windows 10 or Windows Server 2016 systems to match the same keys.
+            $osVersion = Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion' | Select-Object 'ProductName', 'ReleaseId', 'CurrentVersion'
+            if ([System.String]::IsNullOrEmpty($osVersion.ReleaseId))
             {
-                $osVersion | Add-Member -MemberType NoteProperty -Name 'ReleaseId' -Value $osVersion.CurrentVersion
+                $osVersion.ReleaseId = $osVersion.CurrentVersion
             }
-            $osInfo = '{0}, Version {1}' -f $osVersion.ProductName, $osVersion.ReleaseId
+
+            $stringBuilder.AppendFormat('{0}, Version {1}', $osVersion.ProductName, $osVersion.ReleaseId) | Out-Null
         }
         if ([System.Environment]::OSVersion.Platform -eq 'Unix')
         {
-            $osInfo = uname -a
+            # Kernel name, Kenrel release, Kerner version
+            $stringBuilder.AppendFormat('{0} {1} {2}', (uname -s), (uname -r), (uname -v)) | Out-Null
         }
+
+        $stringBuilder.AppendLine() | Out-Null
+        $stringBuilder.AppendLine() | Out-Null
 
         # Get the info about the current logged on user, system and uptime
-        $usrInfo = ''
         if ([System.Environment]::OSVersion.Platform -eq 'Win32NT')
         {
-            $usrInfo = '{0}\{1} on {2} ({3}), Uptime {4:%d} day(s) {4:hh\:mm\:ss}' -f $Env:UserDomain, $Env:Username, $Env:ComputerName.ToUpper(), $PID, [System.TimeSpan]::FromMilliseconds([System.Environment]::TickCount)
+            $stringBuilder.AppendFormat('{0}\{1} on {2} ({3}), Uptime {4:%d} day(s) {4:hh\:mm\:ss}', $Env:UserDomain, $Env:Username, $Env:ComputerName.ToUpper(), $PID, [System.TimeSpan]::FromMilliseconds([System.Environment]::TickCount)) | Out-Null
         }
         if ([System.Environment]::OSVersion.Platform -eq 'Unix')
         {
-
+            $stringBuilder.AppendFormat('{0} on {1} ({2}), {3}', $Env:Username, (hostname), $PID, (uptime).Split(',')[0].Trim()) | Out-Null
         }
 
-        # Show headline
-        $Host.UI.WriteLine($psInfo)
-        $Host.UI.WriteLine($osInfo)
-        $Host.UI.WriteLine()
-        $Host.UI.WriteLine($usrInfo)
-        $Host.UI.WriteLine()
+        $stringBuilder.AppendLine() | Out-Null
+
+        $Host.UI.WriteLine($stringBuilder.ToString())
     }
 }
