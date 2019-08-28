2da1c1db6d2c2aa46ec824b0c82a09a74f73ba89
Claudio Spizzi <claudio@spizzi.net>
2019-08-28 14:54:19 +0200

    Allow multiple worksapces, replace environment variable with default path

--- Modules/ProfileFever/Functions/Workspace/Update-Workspace.ps1
+++ Modules/ProfileFever/Functions/Workspace/Update-Workspace.ps1
@@ -3,6 +3,17 @@
         Update the workspace configuration for Visual Studio Code which is used
         by the extension vscode-open-project.
 
+    .DESCRIPTION
+        By default the path $HOME\Workspace is used to prepare the project list
+        for the vscode-open-project extension. All *.code-workspace files in the
+        root of the path are used for grouped Visual Studio Code workspaces.
+
+    .PARAMETER Path
+        Path to the workspace. $HOME\Workspace is used by default.
+
+    .PARAMETER ProjectListPath
+        Path to the JSON config file of the vscode-open-project extension.
+
     .LINK
         https://marketplace.visualstudio.com/items?itemName=svetlozarangelov.vscode-open-project
 #>
@@ -13,35 +24,47 @@ function Update-Workspace
     (
         [Parameter(Mandatory = $false)]
         [ValidateScript({Test-Path -Path $_})]
-        [System.String]
-        $Path = $Env:Workspace,
+        [System.String[]]
+        $Path = "$HOME\Workspace",
 
         [Parameter(Mandatory = $false)]
         [System.String]
         $ProjectListPath = "$Env:AppData\Code\User\projectlist.json"
     )
 
-    $projectList = @{
-        projects = [Ordered] @{}
-    }
-
-    foreach ($workspace in (Get-ChildItem -Path $Path -Filter '*.code-workspace' -File))
+    begin
     {
-        $projectList.projects.Add(('Workspace {0}' -f $workspace.BaseName), $workspace.FullName)
-    }
-
-    foreach ($group in (Get-ChildItem -Path $Path -Directory))
-    {
-        foreach ($repo in (Get-ChildItem -Path $group.FullName -Directory))
-        {
-            $key = '{0} \ {1}' -f $group.Name, $repo.Name
-
-            $projectList.projects.Add($key, $repo.FullName)
+        $projectList = @{
+            projects = [Ordered] @{}
         }
     }
 
-    if ($PSCmdlet.ShouldProcess($ProjectListPath, 'Update Project List'))
+    process
     {
-        $projectList | ConvertTo-Json | Set-Content -Path $ProjectListPath
+        foreach ($currentPath in $Path)
+        {
+            foreach ($workspace in (Get-ChildItem -Path $currentPath -Filter '*.code-workspace' -File))
+            {
+                $projectList.projects.Add(('Workspace {0}' -f $workspace.BaseName), $workspace.FullName)
+            }
+
+            foreach ($group in (Get-ChildItem -Path $currentPath -Directory))
+            {
+                foreach ($repo in (Get-ChildItem -Path $group.FullName -Directory))
+                {
+                    $key = '{0} \ {1}' -f $group.Name, $repo.Name
+
+                    $projectList.projects.Add($key, $repo.FullName)
+                }
+            }
+        }
+    }
+
+    end
+    {
+        if ($PSCmdlet.ShouldProcess($ProjectListPath, 'Update Project List'))
+        {
+            $projectList | ConvertTo-Json | Set-Content -Path $ProjectListPath
+        }
     }
 }
