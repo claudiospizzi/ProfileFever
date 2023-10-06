89dc1b009db803fc9dbf5c602657c69ea9dd351a
Claudio Spizzi <claudio.spizzi@arcade.ch>
2023-10-06 13:54:41 +0200

    Add the volume summary

--- ProfileFever/Functions/Troubleshooting/Show-SystemSummary.ps1
+++ ProfileFever/Functions/Troubleshooting/Show-SystemSummary.ps1
@@ -60,6 +60,7 @@ function Show-SystemSummary
             $cimComputerSystem  = Get-CimInstance -ClassName 'Win32_ComputerSystem' -Property 'Name', 'Domain', 'NumberOfLogicalProcessors'
             $cimOperatingSystem = Get-CimInstance -ClassName 'Win32_OperatingSystem' -Property 'Caption', 'Version', 'OSLanguage', 'OSArchitecture', 'LastBootUpTime', 'TotalVisibleMemorySize'
             $cimVolumeSystem    = Get-CimInstance -ClassName 'Win32_Volume' -Filter "DriveLetter = 'C:'" -Property 'FreeSpace', 'Capacity'
+            $cimVolumeAll       = Get-CimInstance -ClassName 'Win32_Volume' -Property 'Capacity'
             $cimPageFile        = Get-CimInstance -ClassName 'Win32_PageFileUsage' -Property 'CurrentUsage', 'AllocatedBaseSize'
             $processCount       = (Get-Process).Count
             $connectionCount    = (netstat.exe -n).Count - 4    # Subtract the netstat header
@@ -82,6 +83,7 @@ function Show-SystemSummary
                 ProcessorCount  = $cimComputerSystem.NumberOfLogicalProcessors
                 SystemDiskUsage = ($cimVolumeSystem.Capacity - $cimVolumeSystem.FreeSpace) / $cimVolumeSystem.Capacity
                 SystemDiskSize  = $cimVolumeSystem.Capacity / 1GB
+                AllDiskSize     = ($cimVolumeAll | Measure-Object -Property 'Capacity' -Sum | Select-Object -ExpandProperty 'Sum') / 1GB
                 MemoryUsage     = (($cimOperatingSystem.TotalVisibleMemorySize / 1KB) - $perfCounter.CounterSamples.Where({$_.Path -like '\\*\Memory\Available MBytes'}).CookedValue) / ($cimOperatingSystem.TotalVisibleMemorySize / 1KB)
                 MemorySize      = $cimOperatingSystem.TotalVisibleMemorySize / 1MB
                 PageUsage       = $cimPageFile.CurrentUsage / $cimPageFile.AllocatedBaseSize
@@ -106,6 +108,15 @@ function Show-SystemSummary
         $summaryDiskUsed = Format-HostText -Message ('{0:0.0%}' -f $queryResult.SystemDiskUsage) -ForegroundColor (Get-Color -Value $queryResult.SystemDiskUsage -WarningThreshold 0.8 -ErrorThreshold 0.9)
         $summaryDiskUsed = '{0} of {1:0}GB' -f $summaryDiskUsed, $queryResult.SystemDiskSize
 
+        # If the all disk size is greater than the system disk (plus 2GB for the
+        # the EFI boot and recovery partition), show it after the system disk
+        # size in ths system information summary.
+        $summaryDiskAllSizeText = ''
+        if ($queryResult.AllDiskSize -gt ($queryResult.SystemDiskSize + 2))
+        {
+            $summaryDiskAllSizeText = '+{0:0}GB' -f ($queryResult.AllDiskSize - $queryResult.SystemDiskSize)
+        }
+
         $summaryMemoryUsage = Format-HostText -Message ('{0:0%}' -f $queryResult.MemoryUsage) -ForegroundColor (Get-Color -Value $queryResult.MemoryUsage -WarningThreshold 0.7 -ErrorThreshold 0.85)
         $summaryMemoryUsage = '{0} of {1:0}GB' -f $summaryMemoryUsage, $queryResult.MemorySize
 
@@ -117,7 +128,7 @@ function Show-SystemSummary
         [void] $summary.AppendFormat('Welcome to PowerShell {0}.{1} on {2}', $queryResult.PowerShell.Major, $queryResult.PowerShell.Minor, $queryResult.OperatingSystem.Name).AppendLine()
         [void] $summary.AppendLine()
         [void] $summary.AppendFormat('  System information as of {0:dd.MM.yyyy HH:mm:ss zzz}', $queryResult.Timestamp).AppendLine()
-        [void] $summary.AppendFormat('   {0} 󰓯 {1}  {2}  {3} 󰚗 {4:0}GB 󰋊 {5:0}GB', $queryResult.OperatingSystem.Version, $queryResult.OperatingSystem.Architecture, $queryResult.OperatingSystem.Language, $queryResult.ProcessorCount, $queryResult.MemorySize, $queryResult.SystemDiskSize).AppendLine()
+        [void] $summary.AppendFormat('   {0}  󰓯 {1}   {2}   {3}  󰚗 {4:0}GB  󰋊 {5:0}GB{6}', $queryResult.OperatingSystem.Version, $queryResult.OperatingSystem.Architecture, $queryResult.OperatingSystem.Language, $queryResult.ProcessorCount, $queryResult.MemorySize, $queryResult.SystemDiskSize, $summaryDiskAllSizeText).AppendLine()
         [void] $summary.AppendLine()
         [void] $summary.AppendFormat('    System load   : {0,-40:0}     Hostname      : {1}', $summarySystemLoad, $queryResult.Hostname).AppendLine()
         [void] $summary.AppendFormat('  󰚗  Memory usage  : {0,-40:0}     Processes     : {1}', $summaryMemoryUsage, $queryResult.ProcessCount).AppendLine()
@@ -129,6 +140,7 @@ function Show-SystemSummary
         Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '  ¦ Measure-System            Measure-Memory            Measure-Session' -AppendLine
         [void] $summary.AppendLine()
         Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '    System Auditing' -AppendLine
+        Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '  ¦ Install-Module -Name "SecurityFever" -Repository "PSGallery" -Force' -AppendLine
         Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '  ¦ Get-SystemAuditFileSystem         Get-SystemAuditPowerCycle' -AppendLine
         Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '  ¦ Get-SystemAuditGroupPolicy        Get-SystemAuditUserSession' -AppendLine
         Format-HostText -StringBuilder $summary -ForegroundColor $colorDarkGray -Message '  ¦ Get-SystemAuditMsiInstaller       Get-SystemAuditWindowsService' -AppendLine
