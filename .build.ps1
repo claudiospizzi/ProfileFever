27bf956356cfe343d9444a97cf281f8f7b34d067
Claudio Spizzi <claudio.spizzi@arcade.ch>
2024-09-09 22:10:59 +0200

    Fix build script, add token callback

--- .build.ps1
+++ .build.ps1
@@ -3,8 +3,8 @@
 . InvokeBuildHelperTasks
 
 # Build configuration
-$IBHConfig.RepositoryTask.Token = Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)'
-$IBHConfig.GalleryTask.Token    = Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)'
+$IBHConfig.RepositoryTask.TokenCallback = { Use-VaultSecureString -TargetName 'GitHub Token (claudiospizzi)' }
+$IBHConfig.GalleryTask.TokenCallback    = { Use-VaultSecureString -TargetName 'PowerShell Gallery Key (claudiospizzi)' }
 
 # Special config for the analyzer
 $IBHConfig.AnalyzerTestTask.ScriptAnalyzerRules = Get-ScriptAnalyzerRule | Where-Object { $_.RuleName -notin 'PSReviewUnusedParameter', 'PSAvoidGlobalVars', 'PSAvoidGlobalFunctions', 'PSAvoidUsingWriteHost' }
